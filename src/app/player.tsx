8cc9c3a6b317d41b1395eb5f2a2f078a623d363f
ainochi-kor <kthrkdals@gmail.com>
2024-10-16 01:26:00 +0900

    feat: image view

--- src/app/player.tsx
+++ src/app/player.tsx
@@ -1,13 +1,39 @@
-import { screenPadding } from '@/constants/tokens'
+import { unknownTrackImageUri } from '@/constants/images'
+import { colors, screenPadding } from '@/constants/tokens'
 import { defaultStyles } from '@/styles'
 import React from 'react'
-import { StyleSheet, View } from 'react-native'
+import { ActivityIndicator, StyleSheet, View } from 'react-native'
+import FastImage from 'react-native-fast-image'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import { useActiveTrack } from 'react-native-track-player'
 
 const PlayerScreen = () => {
+	const { top, bottom } = useSafeAreaInsets()
+	const activeTrack = useActiveTrack()
+
+	if (!activeTrack) {
+		return (
+			<View style={[defaultStyles.container, { justifyContent: 'center' }]}>
+				<ActivityIndicator color={colors.icon} />
+			</View>
+		)
+	}
+
 	return (
 		<View style={styles.overlayContainer}>
-			<DismissPlayerSymbol></DismissPlayerSymbol>
+			<DismissPlayerSymbol />
+			<View style={{ flex: 1, marginTop: top + 70, marginBottom: bottom }}>
+				<View style={styles.artworkImageContainer}>
+					<FastImage
+						source={{
+							uri: activeTrack.artwork ?? unknownTrackImageUri,
+							priority: FastImage.priority.high,
+						}}
+						resizeMode="cover"
+						style={styles.artworkImage}
+					/>
+				</View>
+			</View>
 		</View>
 	)
 }
@@ -47,5 +73,22 @@ const styles = StyleSheet.create({
 		...defaultStyles.container,
 		paddingHorizontal: screenPadding.horizontal,
 		backgroundColor: 'rgba(0, 0, 0, 0.5)',
+	},
+	artworkImageContainer: {
+		shadowOffset: {
+			width: 0,
+			height: 8,
+		},
+		shadowOpacity: 0.44,
+		shadowRadius: 11.0,
+		flexDirection: 'row',
+		justifyContent: 'center',
+		height: '45%',
+	},
+	artworkImage: {
+		width: '100%',
+		height: '100%',
+		resizeMode: 'cover',
+		borderRadius: 12,
 	},
 })
